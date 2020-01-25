2064b82fc1d53077f45bb36bd506ae196544ec83
ZTex275 <airflow275@gmail.com>
2020-01-25 12:48:08 +0300

    Добавьте файлы проекта.

---
+++ Models/Repository/EFDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Data.Entity;
+
+namespace GameStore.Models.Repository
+{
+    public class EFDbContext : DbContext
+    {
+        public EFDbContext()
+            : base(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\GameStore\App_Data\GameStore.mdf;Integrated Security=True")
+        {  }
+
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
